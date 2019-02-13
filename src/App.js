a5779bd61714370f0b6ec9f5495952a49d335f99
justindavisa <justindavisa@gmail.com>
2019-02-13 16:34:26 -0500

    Initial commit, App.js initial setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
 import React from 'react';
 
-class App extends React.Component {
+const toDos = [
+  {
+    text: 'Take Out Garbage'
+  }
+];
+
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      toDos: toDos,
+      text: ''
+    }
+  }
+
+  addToDo = e => {
+    e.preventDefault();
+    const newToDo = {
+      text: this.addToDo.state.text
+    };
+
+    this.setState({
+      toDos: [...this.state.toDos, newToDo],
+      text: ''
+    });
+  }
+
+  changeHandler = e => {
+    this.setState({
+      [e.target.id]: e.target.value
+    });
+  }
+
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <
       </div>
     );
   }
